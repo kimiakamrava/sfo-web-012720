5fb34ca076aabd4d911527ae4c058e1526da6180
Levi Miller <paullevimiller@gmail.com>
2020-02-18 16:14:29 -0800

    Add Lecture 15 live notes

--- 15-sinatra-forms-and-rest/game-ratings/app/controllers/games_controller.rb
+++ 15-sinatra-forms-and-rest/game-ratings/app/controllers/games_controller.rb
@@ -1,39 +1,53 @@
 class GamesController < ApplicationController
+	# index
 	get '/games' do
 		@games = Game.all
 		erb :"/games/index"
 	end
 
+	# new
 	get '/games/new' do
 		erb :'/games/new'
 	end
 
+	# show
 	get '/games/:id' do
 		@game = Game.find(params[:id])
 		erb :"/games/show"
 	end
 
+	# create
 	post '/games' do
-		Game.create(params)
-		redirect "/games"
+		@game = Game.create(params)
+		redirect "/games/#{@game.id}"
 	end
 
+	# edit
 	get '/games/:id/edit' do
 		@game = Game.find(params[:id])
 		erb :'/games/edit'
 	end
 
+	# update
 	patch '/games/:id' do
 		@game = Game.find(params[:id])
-		@game.update(title: params[:title], description: params[:description], image_url: params[:image_url], rating: params[:rating])
+		@game.update(
+			title: params[:title],
+			description: params[:description],
+			rating: params[:rating],
+			image_url: params[:image_url]
+		)
 
 		redirect "/games/#{@game.id}"
 	end
-	
-  delete '/games/:id' do
-		@game = Game.find(params[:id])
-		@game.destroy
 
-		redirect "games"		
-  end
+	# delete
+	delete '/games/:id' do
+		binding.pry
+		@game = Game.find(params[:id])
+		@game.delete
+
+		redirect "/games"
+	end
+
 end
