8bba94135d3e72cbf1e81a5f4617539387a68b9e
Amelie Oller <amelieoller@gmail.com>
2020-03-13 12:13:12 -0700

    Finish toy tale challenge lecture

--- 28-jsdom-toy-tale/src/index.js
+++ 28-jsdom-toy-tale/src/index.js
@@ -1,6 +1,84 @@
-let addToy = false;
-
 document.addEventListener("DOMContentLoaded", () => {
+  toggleToyForm();
+  fetchToys();
+  listenToLikeButtonClicks();
+});
+
+function listenToLikeButtonClicks() {
+  const toyCollection = document.getElementById("toy-collection");
+
+  toyCollection.addEventListener("click", function(event) {
+    if (event.target.className === "like-btn") {
+      // PATCH http://localhost:3000/toys/:id
+      // headers:
+      // {
+      //   "Content-Type": "application/json",
+      //   Accept: "application/json"
+      // }
+
+      // body: JSON.stringify({
+      //   "likes": <new number>
+      // })
+      const toyId = event.target.id;
+      const likePTag = event.target.previousElementSibling;
+      const previousLikes = parseInt(likePTag.innerText.split(" ")[0]);
+      const newToyLikes = previousLikes + 1;
+
+      likePTag.innerText = `${newToyLikes} Likes`;
+
+      fetch(`http://localhost:3000/toys/${toyId}`, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json"
+        },
+        body: JSON.stringify({
+          likes: newToyLikes
+        })
+      });
+    }
+  });
+}
+
+// Fetching all toys
+function fetchToys() {
+  fetch("http://localhost:3000/toys")
+    .then(resp => resp.json())
+    .then(data => showToys(data));
+}
+
+function showToys(toys) {
+  toys.forEach(toy => {
+    displayToy(toy);
+  });
+}
+
+function displayToy(toy) {
+  let h2 = document.createElement("h2");
+  h2.innerText = toy.name;
+
+  let img = document.createElement("img");
+  img.setAttribute("src", toy.image);
+  img.setAttribute("class", "toy-avatar");
+
+  let p = document.createElement("p");
+  p.innerText = `${toy.likes} Likes`;
+
+  let btn = document.createElement("button");
+  btn.setAttribute("class", "like-btn");
+  btn.setAttribute("id", toy.id);
+  btn.innerText = "Like <3";
+
+  let div = document.createElement("div");
+  div.setAttribute("class", "card");
+  div.append(h2, img, p, btn);
+
+  document.querySelector("#toy-collection").append(div);
+}
+
+function toggleToyForm() {
+  let addToy = false;
+
   const addBtn = document.querySelector("#new-toy-btn");
   const toyForm = document.querySelector(".container");
   addBtn.addEventListener("click", () => {
@@ -12,4 +90,4 @@ document.addEventListener("DOMContentLoaded", () => {
       toyForm.style.display = "none";
     }
   });
-});
+}
