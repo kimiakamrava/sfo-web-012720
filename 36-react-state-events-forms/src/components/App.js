31af99a505d731afe782a1a783fb73b565296e7d
Amelie Oller <amelieoller@gmail.com>
2020-03-31 10:26:58 -0700

    Update react state lecture code

--- 36-react-state-events-forms/src/components/App.js
+++ 36-react-state-events-forms/src/components/App.js
@@ -1,30 +1,13 @@
-import React, { Component } from "react";
-import RecipeList from "./RecipeList";
-import RecipeForm from "./RecipeForm";
+import React from 'react';
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      recipes: ["Kimchi Fried Rice", "Fruit", "Whipped Cream"]
-    };
-  }
+function App() {
+  const recipes = [];
 
-  addRecipe = recipeName => {
-    this.setState({
-      recipes: [...this.state.recipes, recipeName]
-    });
-  };
-
-  render() {
-    return (
-      <div>
-        <h1>Recipe List</h1>
-        <RecipeList recipes={this.state.recipes} />
-        {/* <RecipeForm makeNewRecipe={this.addRecipe} /> */}
-      </div>
-    );
-  }
+  return (
+    <div>
+      <h1>Recipe List</h1>
+    </div>
+  );
 }
 
 export default App;
--- 36-react-state-events-forms/src/components/Recipe.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Recipe = (props) => {
-  return <li>{props.recipeName}</li>
-};
-
-export default Recipe;
