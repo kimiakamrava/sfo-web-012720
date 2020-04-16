b6b5a79ce6c8b7e0be610135c068a7498d970af6
Amelie Oller <amelieoller@gmail.com>
2020-04-15 17:58:36 -0700

    Create redux lecture

--- 43-intro-to-redux/src/index.js
+++ 43-intro-to-redux/src/index.js
@@ -1,35 +1,11 @@
-import React, { Component } from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import logo from "./logo.svg";
-import "./App.css";
-import { createStore } from "redux";
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 
-const initialState = { count: 0 };
-
-const reducer = (oldState, action) => {
-  console.log("oldState", oldState, "action", action);
-
-  switch (action.type) {
-    case "UPDATE":
-      return { ...oldState, count: oldState.count + action.amount };
-    case "INCREMENT":
-      return { ...oldState, count: oldState.count + 1 };
-    // case "DECREMENT":
-    //   return { ...oldState, count: oldState.count - 1 };
-    default:
-      return oldState;
-  }
-};
-
-const store = createStore(reducer, initialState);
+import './index.css';
+import './App.css';
+import logo from './logo.svg';
 
 class App extends Component {
-  // state = { count: 0 };
-  componentDidMount() {
-    store.subscribe(() => this.forceUpdate());
-  }
-
   render() {
     return (
       <div className="App">
@@ -41,45 +17,43 @@ class App extends Component {
 }
 
 class Header extends Component {
-  renderDescription = () => {
-    const remainder = store.getState().count % 5;
-    const upToNext = 5 - remainder;
-    return `The current count is less than ${store.getState().count +
-      upToNext}`;
-  };
-
   render() {
     return (
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1 className="App-title">Welcome to React</h1>
-        <h3>{this.renderDescription()}</h3>
       </header>
     );
   }
 }
 
 class Counter extends Component {
+  state = { count: 0 };
+
   increment = () => {
-    // this.setState(prevState => ({ count: prevState.count + 1 }));
-    store.dispatch({ type: "UPDATE", amount: 5 });
+    this.setState((prevState) => ({ count: prevState.count + 1 }));
   };
 
   decrement = () => {
-    // this.setState(prevState => ({ count: prevState.count - 1 }));
-    // store.dispatch({ type: "DECREMENT" });
-    store.dispatch({ type: "UPDATE", amount: -3 });
+    this.setState((prevState) => ({ count: prevState.count - 1 }));
+  };
+
+  renderDescription = () => {
+    const remainder = this.state.count % 5;
+    const upToNext = 5 - remainder;
+    return `The current count is less than ${this.state.count + upToNext}`;
   };
 
   render() {
     return (
       <div className="Counter">
-        <h1>{store.getState().count}</h1>
+        <h1>{this.state.count}</h1>
         <button onClick={this.decrement}> - </button>
         <button onClick={this.increment}> + </button>
+        <h3>{this.renderDescription()}</h3>
       </div>
     );
   }
 }
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById('root'));
--- 44-react-redux/src/redux/store.js
+++
@@ -1,8 +0,0 @@
-import { createStore } from "redux";
-import reducer from "./reducer";
-
-const initialState = { count: 0, text: "hello" };
-
-const store = createStore(reducer, initialState);
-
-export default store;
